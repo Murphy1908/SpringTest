2535a3cb1d6de35a42ad7c8d5331dcc9d35ed79a
Murphy1908 <gz_zzhe@outlook.com>
2018-03-04 21:27:21 +0800

    依赖注入：利用set方法给属性赋值

---
+++ di/src/main/java/com/spring/springdi/di/Student.java
@@ -0,0 +1,15 @@
+package com.spring.springdi.di;
+
+public class Student {
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
---
+++ di/src/test/java/com/spring/springdi/di/PersonTest.java
@@ -0,0 +1,25 @@
+package com.spring.springdi.di;
+
+
+import org.junit.*;
+import org.junit.Assert.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.springdi.di.Person;
+
+public class PersonTest{
+    //利用 set 方法给对象赋值
+    @Test
+    public void testSet(){
+        //1、启动 spring 容器
+        //2、从 spring 容器中取出数据
+        //3、通过对象调用方法
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person) context.getBean("person");
+        System.out.println(person.getPname());//vae
+        person.setPname("Murphy");
+        System.out.println(person.getPname());//vae
+    }
+}
