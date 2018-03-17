60459a2016f8b6dcb47c8589e09bfbc9ece1f457
Murphy1908 <gz_zzhe@outlook.com>
2018-03-17 22:04:42 +0800

    全方法展示

--- aopmethod/src/main/java/com/spring/aopmethod/MyAspect.java
+++ aopmethod/src/main/java/com/spring/aopmethod/MyAspect.java
@@ -9,7 +9,7 @@ public class MyAspect {
      * JoinPoint 能获取目标方法的一些基本信息
      * @param joinPoint
      */
-/*    public void myBefore(JoinPoint joinPoint){
+    public void myBefore(JoinPoint joinPoint){
         System.out.println("前置通知 ： " + joinPoint.getSignature().getName());
     }
      
@@ -19,7 +19,7 @@ public class MyAspect {
      
     public void myAfter(){
         System.out.println("最终通知");
-    }*/
+    }
     
     public void myAfterThrowing(JoinPoint joinPoint,Throwable e){
         System.out.println("抛出异常通知 ： " + e.getMessage());
